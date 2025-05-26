e00717db80b02ec00a89d8c7e2843f0ac0f332f8
Steven Hobs <47906512+stevenhobs@users.noreply.github.com>
2025-05-27 02:24:11 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -1,8 +1,3 @@
-console.log(
-    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
-    'color:orangered;font-weight:bolder'
-)
-
 // very important, if you don't know what it is, don't touch it
 // 非常重要，不懂代码不要动，这里可以解决80%的问题，也可以生产1000+的bug
 const hookClick = (e) => {
@@ -24,6 +19,8 @@ const hookClick = (e) => {
 }
 
 document.addEventListener('click', hookClick, { capture: true })
+document.querySelector('#ice-container #content>div').style.cssText = 'height: 94vh; margin: 0 auto;'
+
 
 // css filter
 document.addEventListener('DOMContentLoaded', () => {
